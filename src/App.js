b23f090043222b18520e82d78eff80a51f2f72f7
Princesa_Da_Sombra <francoise.pinceau@gmail.com>
2022-12-29 13:07:24 +0100

    Affichage responsive des 2 template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Dessin from "./component/dessin";
+import './component/Style.css';
+
 
 function App() {
-  return 
+  return <>
+    <Dessin label='Toad'/>
+    <Dessin label='Santa'/>
+  </>
 }
 
 export default App;
---
+++ src/component/dessin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './Style.css';
+
+
+export default function Dessin({label}){
+    return <div className={label}></div>
+}
